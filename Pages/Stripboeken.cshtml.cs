de698184f9809494a5e3bb254aa4244d086e4243
unknown <roanj03@gmail.com>
2022-03-18 17:44:24 +0100

    In deze versie zijn alle paginas aangemaakt waarin iedereen kan werken, ook kan je nu navigeren naar de verschillende paginas.

---
+++ Pages/Stripboeken.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectComicBook.Pages
+{
+    public class Stripboeken : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
