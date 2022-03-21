90e9cca5a4cfcd1b8cfde17407aa91eb6600c912
Khalid <48758660+khalido1990@users.noreply.github.com>
2022-03-21 15:08:46 +0100

    Added create method for author. renamed pages.

---
+++ Pages/Authors.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectComicBook.Models;
+
+namespace ProjectComicBook.Pages
+{
+    public class auteurs : PageModel
+    {
+        private DatabaseHandler DatabaseHandler = new DatabaseHandler();
+        public void OnGet()
+        {
+            
+        }
+
+        public RedirectToPageResult OnPostAddAuthor()
+        {
+            var authorName = Request.Form["authorName"];
+            string authorDescription = Request.Form["authorDescription"];
+            DatabaseHandler.AddAuthor(authorName, authorDescription);
+            return RedirectToPage("./Index");
+        }
+    }
+}
