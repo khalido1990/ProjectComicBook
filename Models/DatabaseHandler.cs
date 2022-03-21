94388d20b467f12e7ddb1659ed93c65c615e1aba
Marco Boschma <marcoboschma@live.nl>
2022-03-21 15:17:51 +0100

    Een test commit niks mee doen

--- Models/DatabaseHandler.cs
+++ Models/DatabaseHandler.cs
@@ -28,5 +28,16 @@ namespace ProjectComicBook.Models
             using var connection = Connect();
             connection.Execute("DELETE FROM author WHERE authorID = @authorID", new {authorID});
         }
+        public void AddNewUser(string UserName, string Password)
+        {
+            using var connection = Connect();
+            //var insert = ("Insert INTO user (@username, @password), VALUES (@0, @1)");
+            var sqlinsert = "INSERT INTO user (username,password)" +"VALUES (@0,@1)";
+            string querding = new string(UserName + "," + Password);
+            connection.Execute(sqlinsert, querding);
+            //connection.Execute(insert, "UserName", "Password");
+            //connection.Execute("INSERT INTO user (username, password)", VALUES ("UserName", "Password"));
+
+        }
     }
 }
--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySqlX.XDevAPI.Relational;
+using ProjectComicBook.Models;
 
 namespace ProjectComicBook.Pages
 {
     public class Login : PageModel
     {
+    
+        [Required] [BindProperty] public string UserName { get; set; }
+        [Required] [BindProperty] public string Password { get; set; }
+        public DatabaseHandler Handler = new DatabaseHandler();
+        
         public void OnGet()
         {
             
         }
+
+        public void OnPostLogin(string UserName, string Password)
+        {
+            
+        }
+
+        public void OnPostReg(string UserName, string Password)
+        {
+            Handler.AddNewUser(UserName, Password);
+        }
+
+
     }
 }
