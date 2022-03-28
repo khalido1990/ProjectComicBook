6ed55101157b7dd6ade376d2fa94aac0bcdeb861
Marco Boschma <marcoboschma@live.nl>
2022-03-28 10:50:42 +0200

    Merged all of my branches exept for the html

--- Models/DatabaseHandler.cs
+++ Models/DatabaseHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using Dapper;
 using MySql.Data.MySqlClient;
+using ProjectComicBook.Pages;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProjectComicBook.Models
 {
@@ -90,6 +92,93 @@ namespace ProjectComicBook.Models
         {
             using var connection = Connect();
             connection.Execute("DELETE FROM comicbook WHERE comicbookID = @comicbookID", new {comicbookID});
+        }
+        
+        public void AddNewUser(string username, string password, string name)
+        {
+            int role_ID = 3;
+            using var connection = Connect();
+            //var insert = ("Insert INTO user (@username, @password), VALUES (@0, @1)");
+            connection.Execute("INSERT INTO user (username,password, name, role_ID)" +"VALUES (@username,@password, @name, @role_ID)", 
+                new {username, password, name, role_ID});
+        }
+
+        public bool CheckForDoubleEntryUserName(string userName)
+        {
+            using var connection = Connect();
+            try
+            {
+                connection.Query(
+                    "SELECT * FROM user WHERE username=@userName",
+                    new {userName});
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            
+        }
+        public bool CheckForDoubleEntryName(string Name)
+        {
+            using var connection = Connect();
+            try
+            {
+                connection.Query(
+                    "SELECT * FROM user WHERE name=@Name",
+                    new {Name}).Single();
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            
+        }
+        
+        public bool CheckForDoubleEntry(string userName, string Name)
+        {
+            using var connection = Connect();
+            try
+            {
+                connection.Query(
+                    "SELECT * FROM user WHERE username=@userName or name=@Name",
+                    new {userName, Name});
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            
+        }
+        public user GetUserNow(string username, string password)
+        {
+            var passwordhasher = new PasswordHasher<string>();
+            user _user = new user();
+            using var connection = Connect();
+            try
+            {
+                _user = connection.Query<user>(
+                    "SELECT * FROM user WHERE username=@username",
+                    new {username}).Single();
+                if (passwordhasher.VerifyHashedPassword(null, _user.password, password) ==
+                    PasswordVerificationResult.Success)
+                {
+                    return _user;
+                }
+
+            }
+            catch (Exception e)
+            {
+                return new user();
+            }
+
+            return new user();
+
         }
     }
 }
--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,12 +1,158 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectComicBook.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProjectComicBook.Pages
 {
-    public class Login : PageModel
+    
+    public class user
     {
-        public void OnGet()
+        public int userID { get; set; }
+        public int role_ID { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public bool StayLoggedIn { get; set; }
+
+        public user()
         {
             
         }
+        public user(string userName, string password, bool keepLoggedIn)
+        {
+            this.username = userName;
+            this.password = password;
+            StayLoggedIn = keepLoggedIn;
+        }
+        
+    }
+
+    public class MyCookie
+    {
+        public string Password;
+        public string UserName;
+        public bool StayLoggedIn;
+
+        public MyCookie(string UserName, string Password, bool StayLoggedIn)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+            this.StayLoggedIn = StayLoggedIn;
+        }
+    }
+    public class Login : PageModel
+    {
+    
+        [Required] [BindProperty] [MaxLength(25)] public string UserName { get; set; }
+        
+        [Required] [BindProperty] [MaxLength(25)] public string Password { get; set; }
+        [Required] [BindProperty] [MaxLength(25)] public string Name { get; set; }
+        [TempData, Required, Compare(nameof(Password))] public string Cpassword { get; set; }
+        [TempData] public bool KeepLoggedIn { get; set; }
+        public string ErrorMsg = "";
+        private user _user;
+        
+
+        public user SessionUser
+        {
+            get
+            {
+                return this._user;
+            }
+            set
+            {
+                if (CheckCookie())
+                {
+                    _user = Setup();
+                }
+            }
+        }
+        
+        public DatabaseHandler Handler = new DatabaseHandler();
+
+
+        public void OnGet()
+        {
+            SessionUser = new user();
+        }
+
+        public void OnPostLogout()
+        {
+            DeleteCookies();
+        }
+        public void OnPostLogin(string UserName, string Password, bool KeepLoggedIn)
+        {
+            user me = Handler.GetUserNow(UserName, Password);
+            if (me.username == UserName)
+            {
+                me.StayLoggedIn = KeepLoggedIn;
+                MyCookie cookie = new MyCookie(UserName, Password, KeepLoggedIn);
+                DeleteCookies();
+                Response.Cookies.Append("Login", JsonConvert.SerializeObject(cookie), new CookieOptions());
+                ErrorMsg = MakeMsg("Welcome" + " " + me.username);
+
+            }
+            else
+            {
+                ErrorMsg = MakeMsg("Wrong username or password");
+            }
+        }
+
+        public void DeleteCookies()
+        {
+            Response.Cookies.Delete("Login");
+        }
+        public user Setup()
+        {
+            MyCookie cookie = JsonConvert.DeserializeObject<MyCookie>(Request.Cookies["Login"]);
+            if (Request.Cookies["Login"] != null)
+            {
+                user me = Handler.GetUserNow(cookie.UserName, cookie.Password);
+                return me;
+            }
+
+            return new user();
+        }
+
+        public bool CheckCookie()
+        {
+            if (Request.Cookies["Login"] != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void OnPostReg(string UserName, string Password, string Name, string Cpassword)
+        {
+            
+            if (!Handler.CheckForDoubleEntry(UserName, Name))
+            {
+                Handler.AddNewUser(UserName, HashVal(Password), Name);
+                ErrorMsg = MakeMsg("Welcome" + " " + Name);
+            }
+            else
+            {
+                ErrorMsg = MakeMsg(Handler.CheckForDoubleEntryName(Name) ? "Name already taken" : "Username already taken");
+            }
+            
+        }
+
+        public string HashVal(string pass)
+        {
+            var passHasher = new PasswordHasher<string>();
+            return pass = passHasher.HashPassword(null,pass);
+        }
+        private string MakeMsg(string msg)
+        {
+            return msg;
+        }
+
+
     }
 }
